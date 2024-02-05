8bb28caa24a35bdc742a97f3416ba109f48b43d8
MarcelSecco1 <marcelsecco1@gmail.com>
2024-02-05 04:48:02 -0300

    start: first commit

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,8 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
-import Navbar from "@/components/Navbar";
-
-// const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>Home</title>
-      </Head>
-      <div>
-        <h1>Home</h1>
-      </div>
-    </>
+    <div>
+      <h1>Home</h1>
+      <p>Welcome to the Next.js app!</p>
+    </div>
   );
 }
